988e4a14801e7336bddfbe3ab1c03dca70aae06c
oyindamola <oyindamoladawodu22@gmail.com>
2022-02-24 16:27:06 +0100

    backend set

--- server/index.js
+++ server/index.js
@@ -1,39 +1,20 @@
-const express = require('express');
-const { Client } = require('pg')
-const cors = require('cors');
-const port = 3002
+const app = require("express")();
+const server = require("http").createServer(app);
 
-const db = new Client({
-    host: "localhost",
-    user: "postgres",
-    port: 4000,
-    password: "ADMIN",
-    database: 'moviedb'
-})
-
-db.connect();
-
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// CORS implemented so that we don't get errors when trying to access the server from a different server location
-app.use(cors());
-
-// GET: Fetch all movies from the database
-app.get('/', (req, res) => {
-    db.query(`Select * from videos`)
-        .then((data) => {
-            console.log(data.rows);
-            res.json(data.rows);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
 });
 
+io.on("connection", (socket) => {
+  console.log("connection made successfully");
+  socket.on("message", (payload) => {
+    console.log("Message received on server: ", payload);
+    io.emit("message", payload);
+  });
+});
 
-app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-  })
+server.listen(7000, () => {
+  console.log("I am listening at port: 7000)");
+});
